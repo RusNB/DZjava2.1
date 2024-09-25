fad4032d2a9c662b8494e59bf770ba68fe5b10b5
RusNB <balushkin-007@mail.ru>
2024-09-25 22:29:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int amount = 16800; // стоимость билета
+        int x = 20; // 20р = 1 миля
+
+        System.out.println((amount / x) + " милей");
+    }
+}
