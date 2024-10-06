622295f024e9574bd4aae4bdae89f4e686647455
RusNB <balushkin-007@mail.ru>
2024-10-06 20:23:56 +0300

    code editing

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         int price = 16_800; // стоимость билета
+
         int miles = price / 20;
 
         System.out.println(" Бонус " + miles);
